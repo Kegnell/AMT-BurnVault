a4d1728feff4c4fb5a666b0fb019fc685c7e5bdb
Kegnell <diego.pereira.pck@gmail.com>
2023-06-20 17:14:53 -0300

    add check return type for M02 issue

--- burnVault.sol
+++ burnVault.sol
@@ -9,25 +9,18 @@ contract BurnVault is Ownable {
     address constant addrBtcb = 0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c;
     address constant addrAmt = 0x6Ae0A238a6f51Df8eEe084B1756A54dD8a8E85d3;
 
-    //For production
-    /*
+
     IERC20 constant btcb = IERC20(addrBtcb);
     AMT constant amt = AMT(addrAmt);
-    */
 
-    //For testing
-    IERC20  btcb;
-    AMT  amt;
-    constructor(address _amt, address _btcb){
-        btcb = IERC20(_btcb);
-        amt = AMT(_amt);
-    }
+    constructor(){}
    
     function backingWithdraw(uint256 amount) public {
         uint256 totalSupply = amt.totalSupply();
-        amt.burnFrom(msg.sender, amount);
         uint256 btcbToTransfer = (amount * btcb.balanceOf(address(this)))/totalSupply;
-        btcb.transfer(msg.sender, btcbToTransfer);
+        amt.burnFrom(msg.sender, amount);
+        bool btcbTransferSuccess = btcb.transfer(msg.sender, btcbToTransfer);
+        require(btcbTransferSuccess,"Transaction failed");
         emit burnMade(amount, btcbToTransfer);
     }
 }
